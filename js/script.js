323b13acc05dfa576738afa3c228d6d511fe79f8
blackat <eugenio_lentini@fastmail.fm>
2014-12-13 09:55:11 +0100

    changed png image with svg

--- js/script.js
+++ js/script.js
@@ -24,8 +24,15 @@ boxApp.controller('boxCtrl', function ($scope) {
 
             // Called on a failed authentication
             failure: function (theUser, errorString) {
-                // Print some info to the log
                 console.log("Error authenticating: " + errorString);
+                if (errorString.indexOf('invalid_grant') > -1) {
+                    $scope.errorMessage = "User or password incorrect"
+                    $scope.messageClass = "alert alert-danger";
+                    $scope.$apply();
+                } else {
+                    $scope.errorMessage = errorString;
+                    $scope.$apply();
+                }
             }
         })
     };
@@ -68,87 +75,3 @@ boxApp.controller('boxCtrl', function ($scope) {
         $scope.user.password = "";
     }
 });
-
-
-/*
- var username;
- var passoword;
-
- function signup() {
-
- resetAlert();
-
- username = document.getElementById('inputUsername').value;
- password = document.getElementById('inputPassword').value;
-
- console.log("username: " + username);
- console.log("password: " + password);
-
- // Create the KiiUser object
- var user = KiiUser.userWithUsername(username, password);
-
- // Register the user, defining callbacks for when the process completes
- user.register({
- // Called on successful registration
- success: function(theUser) {
- // Print some info to the log
- console.log("User registered!");
- console.log(theUser);
- document.getElementById('success').innerText = 'User ' + theUser._username + ' succesfully registered.';
- document.getElementById('success-div').style.display = 'block';
- },
- // Called on a failed registration
- failure: function(theUser, errorString) {
- // Print some info to the log
- console.log("Error registering: " + errorString);
- document.getElementById('error').innerText = errorString;
- document.getElementById('error-div').style.display = 'block';
- }
- });
- }
-
- function signin() {
-
- resetAlert();
-
- username = document.getElementById('inputUsername').value;
- password = document.getElementById('inputPassword').value;
-
- console.log("username: " + username);
- console.log("password: " + password);
-
- // Authenticate the user
- KiiUser.authenticate(username, password, {
- // Called on successful registration
- success: function(theUser) {
- // Print some info to the log
- console.log("User authenticated!");
- console.log(theUser);
-
- hideSignForm();
- },
-
- // Called on a failed authentication
- failure: function(theUser, errorString) {
- // Print some info to the log
- console.log("Error authenticating: " + errorString);
- if (errorString === 'invalid_grant') {
- document.getElementById('error').innerText = 'Invalid username or password';
- } else {
- document.getElementById('error').innerText = errorString;
- }
- document.getElementById('error-div').style.display = 'block';
- }
- })
- }
-
- function resetAlert() {
- document.getElementById('error-div').style.display = 'none';
- document.getElementById('success-div').style.display = 'none';
- }
-
- function hideSignForm() {
- document.getElementById('sign-form').style.display = 'none';
- document.getElementById('authenticated').style.display = 'block';
- }
- */
